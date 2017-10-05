fd0ec2b9679cbceeb66de321e175df3a2d86517f
MikeJohnstone2008 <30842339+MikeJohnstone2008@users.noreply.github.com>
2017-10-05 15:34:03 -0700

    update book.js model
    adding attributes

--- api-with-sails/api/models/Book.js
+++ api-with-sails/api/models/Book.js
@@ -8,7 +8,32 @@
 module.exports = {
 
   attributes: {
-
-  }
+    title:  {
+        type:   'string',
+        notNull:    true,
+        maxLength:  30
+    },
+    categories: {
+        type:   'string',
+        maxLength:  true
+    },
+    author_last_name:   {
+        type:   'string',
+        required:    true
+    },
+    first_name: {
+        type:   'string',
+        notNull:    true     
+    },
+    date_published: {
+        type:   'string',
+        maxLength:  10   
+    },
+    other_titles:   {
+        type:   'string',
+        maxLength:  60
+    }
+  },
+  schema: true
 };
 
